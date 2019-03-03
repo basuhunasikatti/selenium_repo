2b7d770df806a958f738b7771487efdfddfd3c03
basuhunasikatti <basuhunasikatti@gmail.com>
2019-03-03 08:37:30 +0530

    Added Timeout

--- SeleniumSessions/src/main/java/com/qa/sessions/LaunchBrowser.java
+++ SeleniumSessions/src/main/java/com/qa/sessions/LaunchBrowser.java
@@ -1,5 +1,7 @@
 package com.qa.sessions;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -12,5 +14,6 @@ public class LaunchBrowser {
 		System.setProperty("webdriver.chrome.driver","D:/AutomationJars/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 	}
 }
